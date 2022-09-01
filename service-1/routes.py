2308359ed4cb43196ede4a1eebaca9cd84dae701
anassumrani <sumranianas@hotmail.com>
2022-09-01 15:20:28 +0000

    git config

--- service-1/routes.py
+++ service-1/routes.py
@@ -1,13 +1,21 @@
+from crypt import methods
 from urllib import response
 from project2 import app
-from flask import Response, request
+from flask import Response, request, redirect, url_for, render_template
 import random
 import requests
+
+
+@app.route('/', methods=["GET"])
+def home_page(self):
+    return render_template('form.html')
+
 
 @app.route('/get/data_age', methods=["GET"])
 def get_data_age(self):
     age = requests.get()
     disease = requests.get()
     doctor = response.text()
+    
 
     
